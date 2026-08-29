0aa0120172c0691bddca8b0cea5cdf966a9ac6cb
PMo-netizen <paulmouland@hotmail.com>
2026-08-29 20:03:30 +0800

    Add indicative pack photography and how-to-use for trade site

--- server.mjs
+++ server.mjs
@@ -20,6 +20,8 @@ const MIME = {
   ".json": "application/json; charset=utf-8",
   ".svg": "image/svg+xml",
   ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
   ".ico": "image/x-icon",
   ".woff2": "font/woff2",
   ".txt": "text/plain; charset=utf-8",
